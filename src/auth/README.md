4dbe22d6d41f6db78b1ec8d2239f158d8b0b3508
serine000 <serine_md@hotmail.com>
2023-10-22 13:24:07 +0300

    implementing login logic and credentials check

--- src/auth/README.md
+++ src/auth/README.md
@@ -5,7 +5,8 @@ are present in the database.
 
 The `init.sql` initializes the `auth` database with a `user` table and inserts within it a customer user we created in that script.
 Commands used:
-- To initialize: mysql -u root -p < init.sql
-- Access mysql: mysql -u root -p [and insert your password]
-- show databases;
-- show tables;
+- To initialize: `mysql -u root -p < init.sql`
+- Access mysql: `mysql -u root -p` [and insert your password]
+- Display databases: `show databases;`
+- Pick a specific database to work with: `use [database name];`
+- Display tables in specific database: `show tables;`
--- src/auth/server.py
+++ src/auth/server.py
@@ -9,7 +9,7 @@ server = Flask(__name__)
 mysql = MySQL(server)
 
 # Database configurations in our server dictionary
-server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST") #localhost
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
@@ -23,6 +23,29 @@ def login():
         return "missing credentials", 401
 
     # Check for username and password in database
+    cursor = mysql.connection.cursor()
+    res = cursor.execute(
+            "SELECT email, password FROM user WHERE email=%s",
+            (auth.username,)
+            )
+    # Row found (should be only 1 row)
+    if res > 0:
+        # result is a tuple
+        user_row = cursor.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return create_jwt(auth.username, os.environ.get("JWT_SECRET"), True)
+
+    else:
+        # user does not exist
+        return "invalid credentials do not exist", 401
+
+
+
 
 
 
