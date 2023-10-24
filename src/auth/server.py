8ef3f8ed3c3728ea289c69fce1afd332fc90359b
serine000 <serine_md@hotmail.com>
2023-10-24 10:32:52 +0300

    adding error handling for database access

--- src/auth/server.py
+++ src/auth/server.py
@@ -1,5 +1,8 @@
-import jwt, datetime, os
-from flask import Flask, request
+import datetime
+import jwt
+import os
+from flask import Flask
+from flask import request
 from flask_mysqldb import MySQL
 
 # Creates a Flask web application instance
@@ -20,34 +23,45 @@ server.route('/login', methods = ["POST"])
 
 
 def login():
+    """
+    Authenticates the user and returns a JWT token if the credentials are valid.
+
+    Returns:
+        str: JWT token if the credentials are valid.
+        str: Error message if the credentials are invalid or missing.
+    """
     auth = request.authorization
     if not auth:
         return "missing credentials", 401
 
-    # Check for username and password in database
-    cursor = mysql.connection.cursor()
-    res = cursor.execute(
-            "SELECT email, password FROM user WHERE email=%s",
-            (auth.username,)
-            )
-    # Row found (should be only 1 row)
-    if res > 0:
-        # result is a tuple
-        user_row = cursor.fetchone()
-        email = user_row[0]
-        password = user_row[1]
+    try:
+        # Check for username and password in database
+        cursor = mysql.connection.cursor()
+        res = cursor.execute(
+                "SELECT email, password FROM user WHERE email=%s",
+                (auth.username,)
+                )
+        # Row found (should be only 1 row)
+        if res > 0:
+            # result is a tuple
+            user_row = cursor.fetchone()
+            email = user_row[0]
+            password = user_row[1]
 
-        if auth.username != email or auth.password != password:
-            return "invalid credentials", 401
+            if auth.username != email or auth.password != password:
+                return "invalid credentials", 401
+            else:
+                return create_jwt(auth.username, os.environ.get("JWT_SECRET"), True)
+
         else:
-            return create_jwt(auth.username, os.environ.get("JWT_SECRET"), True)
+            # user does not exist
+            return "user does not exist", 401
 
-    else:
-        # user does not exist
-        return "invalid credentials do not exist", 401
+    except Exception as e:
+        return str(e), 500
 
 
-def create_jwt(username, secret, authrization):
+def create_jwt(username, secret, authorization):
     return jwt.encode(
             {
                 "username": username,
@@ -55,7 +69,7 @@ def create_jwt(username, secret, authrization):
                         tz = datetime.timezone.utc
                         ) + datetime.timedelta(days = 1),
                 "iat": datetime.datetime.utcnow(),
-                "admin": authrization,
+                "admin": authorization,
                 },
             secret,
             algorithm = "HS256",
